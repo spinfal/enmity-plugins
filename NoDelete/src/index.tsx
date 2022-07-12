f6f69d91d27046f335ee17ccfadb2098499a4c30
Marek Veselý <veselym@spstrutnov.cz>
2022-07-12 20:14:27 +0200

    Who doesn;t like fancy toasts

--- NoDelete/src/index.tsx
+++ NoDelete/src/index.tsx
@@ -1,10 +1,10 @@
 import { Plugin, registerPlugin } from "enmity/managers/plugins";
-import { getByProps } from "enmity/metro";
+import { getByProps, getModule } from "enmity/metro";
 import { create } from "enmity/patcher";
 import manifest from "../manifest.json";
-
+import * as Assets from "enmity/api/assets";
 const Patcher = create("NoDelete");
-
+const Toast = getModule(m => m.open !== undefined && m.close !== undefined && !m.openLazy && !m.startDrag && !m.init && !m.openReplay && !m.openChannelCallPopout)
 const NoDelete: Plugin = {
     ...manifest,
     patches: [],
@@ -33,6 +33,10 @@ const NoDelete: Plugin = {
                 console.log(
                     `NoDelete delayed start attempt ${attempt}/${attempts}.`
                 );
+                Toast.open({
+                    content: `NoDelete start attempt ${attempt}/${attempts}.`,
+                    source: Assets.getIDByName('ic_staff_guild_icon_blurple'),
+                });
                 const MessageDelete =
                     FluxDispatcher._orderedActionHandlers.MESSAGE_DELETE.find(
                         (h) => h.name === "MessageStore"
@@ -86,14 +90,26 @@ const NoDelete: Plugin = {
                     } catch {}
                 });
                 console.log("NoDelete delayed start successful.");
+                Toast.open({
+                    content: `NoDelete delayed start successful.`,
+                    source: Assets.getIDByName('Check'),
+                });
             } catch (e) {
                 if (attempt < attempts) {
                     console.warn(
                         `NoDelete failed to start. Trying again in ${attempt}0s.`
                     );
+                    Toast.open({
+                        content: `NoDelete failed to start trying again in ${attempt}0s.`,
+                        source: Assets.getIDByName('ic_message_retry'),
+                    });
                     setTimeout(plugin, attempt * 10000);
                 } else {
                     console.error(`NoDelete failed to start. Giving up.`);
+                    Toast.open({
+                        content: `NoDelete failed to start. Giving up.`,
+                        source: Assets.getIDByName('Small'),
+                    });
                 }
             }
         };

d11359196087617c817d74a9ddfab633158cb3aa
Marek Veselý <57869222+notmarek@users.noreply.github.com>
2022-07-18 14:35:25 +0200

    test plugin

--- TestPlugin/src/index.tsx
+++ TestPlugin/src/index.tsx
@@ -9,12 +9,41 @@ const ChannelStore = getByProps("getChannel", "getDMFromUserId");
 const Patcher = create("message-spoofer");
 const Opener = getByProps("openLazy");
 const FluxDispatcher = getByProps(
-    "_currentDispatchActionType",
-    "_subscriptions",
-    "_waitQueue"
+  "_currentDispatchActionType",
+  "_subscriptions",
+  "_waitQueue"
 );
 
 const TestPlugin: Plugin = {
-    ...manifest
+  ...manifest,
+  onStart() {
+    Patcher.before(Opener, "openLazy", (_, [component, sheet]) => {
+      if (sheet === "MessageLongPressActionSheet") {
+        component.then((instance) => {
+          let func = instance.default;
+          instance.default = function (
+            { message, user, channel, canAddNewReactions },
+            _
+          ) {
+            let og = func({ message, user, channel, canAddNewReactions }, _);
 
-}
+            let ButtonRow =
+              og.props.children.props.children.props.children[1][0].type;
+
+            try {
+              og.props.children.props.children.props.children[1].slice(4);
+            } catch (e) {}
+            return og;
+          };
+          return instance;
+        });
+      }
+    });
+  },
+  onStop() {
+    Patcher.unpatchAll();
+  },
+  patches: [],
+};
+
+registerPlugin(TestPlugin);

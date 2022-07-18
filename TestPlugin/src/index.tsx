545295683bc67a3de1fa952a958bc027bb90a9c8
Marek Veselý <veselym@spstrutnov.cz>
2022-07-18 16:31:22 +0200

    testing

--- TestPlugin/src/index.tsx
+++ TestPlugin/src/index.tsx
@@ -1,44 +1,26 @@
 import { Plugin, registerPlugin } from "enmity/managers/plugins";
-import { getByProps } from "enmity/metro";
+import { getByDisplayName, getByProps } from "enmity/metro";
 import { Messages, React } from "enmity/metro/common";
 import { create } from "enmity/patcher";
 import * as Assets from "enmity/api/assets";
 import manifest from "../manifest.json";
 const MessageStore = getByProps("getMessage", "getMessages");
 const ChannelStore = getByProps("getChannel", "getDMFromUserId");
-const Patcher = create("message-spoofer");
+const Patcher = create("test-plugin");
 const Opener = getByProps("openLazy");
 const FluxDispatcher = getByProps(
   "_currentDispatchActionType",
   "_subscriptions",
   "_waitQueue"
 );
+const TextInput = getByDisplayName("ChatInput")
 
 const TestPlugin: Plugin = {
   ...manifest,
   onStart() {
-    Patcher.before(Opener, "openLazy", (_, [component, sheet]) => {
-      if (sheet === "MessageLongPressActionSheet") {
-        component.then((instance) => {
-          let func = instance.default;
-          instance.default = function (
-            { message, user, channel, canAddNewReactions },
-            _
-          ) {
-            let og = func({ message, user, channel, canAddNewReactions }, _);
-
-            let ButtonRow =
-              og.props.children.props.children.props.children[1][0].type;
-
-            try {
-              og.props.children.props.children.props.children[1].slice(4);
-            } catch (e) {}
-            return og;
-          };
-          return instance;
-        });
-      }
-    });
+    Patcher.before(TextInput, 'default', (...args) => {
+      console.log(args);
+    })
   },
   onStop() {
     Patcher.unpatchAll();

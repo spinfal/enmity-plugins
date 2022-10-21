5f59240c91d5475cfe24b2b54c4d65e5cfff35e8
spinfal <spin@seized.email>
2022-10-21 14:39:22 -0700

    fix tiktok.com regex

--- BetterTiktokEmbeds/src/index.tsx
+++ BetterTiktokEmbeds/src/index.tsx
@@ -25,9 +25,9 @@ const BTE: Plugin = {
 
             Patcher.before(Messages, "sendMessage", (self, args, orig) => {
                 const content = args[1]["content"];
-                const tiktokLinks = content.match(/^http(s)?:\/\/(www.)?tiktok.com\/(@[a-zA-Z0-9_.]{2,24}\/video\/\d+|\w{1}\/[a-zA-Z0-9_.-]{8,12})(\/)?$/gim);
+                const tiktokLinks = content.match(/http(s)?:\/\/(www.)?tiktok.com\/(@[a-zA-Z0-9_.]{2,24}\/video\/\d+|\w{1}\/[a-zA-Z0-9_.-]{8,12})(\/)?/gim);
                 if (!tiktokLinks) return;
-                args[1]["content"] = (get('_tiktok', '_type', false) === 'tt-embed.com' ? content.replace(/^http(s)?:\/\/(www.)?tiktok.com/gim, `https://tt-embed.com/?q=https://tiktok.com`) : content.replace(/http(s)?:\/\/(www.)?tiktok.com/gim, `https://vm.dstn.to`));
+                args[1]["content"] = (get('_tiktok', '_type', false) === 'tt-embed.com' ? content.replace(/http(s)?:\/\/(www.)?tiktok.com/gim, `https://tt-embed.com/?q=https://tiktok.com`) : content.replace(/http(s)?:\/\/(www.)?tiktok.com/gim, `https://vm.dstn.to`));
             });
         } catch (err) {
             console.log('[ BetterTiktokEmbeds Error ]', err);

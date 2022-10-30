7710c44306812a9f25ef15a8717d9199eb866583
spinfal <spin@seized.email>
2022-10-30 00:12:54 -0700

    fix plugin updater

--- common/components/_pluginSettings/utils/update.ts
+++ common/components/_pluginSettings/utils/update.ts
@@ -14,14 +14,14 @@ async function check_for_updates({ manifest }: Props) {
     const res = await REST.get(url);
     const content = await res.text;
 
-    // get the version from the source
+    // get the version and build from the source
     let external_version = content.match(/\d\.\d\.\d+/g);
-    let external_build = content.match(/patch\-\d\.\d\.\d+/g);
-    if (!external_version || !external_build) return;
+    let external_build = content.match(/patch\-\d\.\d\.\d+/g)
+    if (!external_version || !external_build) return no_updates(manifest['name'], manifest['version']);
 
     // set the versions to their index
-    external_version = external_version[0];
-    external_build = external_build[0];
+    external_version = external_version[0]
+    external_build = external_build[0]
 
     /* 
     i dont need to match specific parts of the version, here are some tests~
@@ -33,9 +33,9 @@ async function check_for_updates({ manifest }: Props) {
     */
 
     // if the version is not the current one, that means its newer, otherwise run the no update function
-    if (external_version != manifest['verson']) return show_update_dialog(url, external_version, external_build.split('-')[1], manifest, false)
+    if (external_version != manifest['version']) return show_update_dialog(url, external_version, external_build.split('-')[1], manifest, false)
     if (external_build != manifest['build']) return show_update_dialog(url, external_version, external_build.split('-')[1], manifest, true)
-    return no_updates(manifest['name'], manifest['verson'])
+    return no_updates(manifest['name'], manifest['version'])
 }
 
 const show_update_dialog = (url: string, version: string, build: string, manifest: object, is_ghost_patch: boolean) => {
@@ -48,41 +48,42 @@ const show_update_dialog = (url: string, version: string, build: string, manifes
         cancelText: "Not now",
 
         // run the install function
-        onConfirm: () => install_plugin(url, type, is_ghost_patch, manifest),
+        onConfirm: () => install_plugin(url, type, manifest, is_ghost_patch),
     });
 }
 
-const no_updates = (name: string, version: string) => {
+const no_updates = (name: string, type: string) => {
     // logs the fact that youre on the latest version with both a toast a
-    console.log(`[${name}] is on the latest version (${version})`)
-    Toasts.open({ content: `${name} is on the latest version (${version})`, source: Icons.Settings.Toasts.Settings });
+    console.log(`[${name}] Plugin is on the latest version, which is ${type}`)
+    Toasts.open({ content: `${name} is on latest version (${type})`, source: Icons.Settings.Toasts.Settings });
 }
 
-async function install_plugin(url: string, type: string, is_ghost_patch: boolean, manifest: object) {
+async function install_plugin(url: string, type: string, manifest: object, is_ghost_patch: boolean) {
     //@ts-ignore
     window.enmity.plugins.installPlugin(url, ({ data }) => {
-        console.log(`${manifest['name']} Update Error`, data);
         // as a callback, waits for a success of "installed-plugin" or "overriden-plugin"
         // before showing the dialog to reload discord
-        data == "installed_plugin" || data == "overridden_plugin" ? Dialog.show({
-            title: `Updated ${manifest['name']}`,
-            body: `Successfully updated to ${is_ghost_patch ? `build` : `version` } \`${type}\`. \nWould you like to reload Discord now?`,
-            confirmText: "Yep!",
-            cancelText: "Later",
-            // reload discord from native function
-            onConfirm: () => { reload() },
-        }) : Dialog.show({
-            title: "Error",
-            body: `Something went wrong while updating ${manifest['name']}.`,
-            confirmText: "Report this issue",
-            cancelText: "Cancel",
-            onConfirm: () => { Dialog.close() },
-            onCancel: () => {
-                // @ts-ignore
-                Router.openURL(`https://github.com/spinfal/enmity-plugins/issues/new?assignees=&labels=bug&template=bug_report.md&title=%5BBUG%5D%20${manifest['name']}%20Update%20Error%3A%20${!is_ghost_patch ? `v${version}` : `b${version}`}`);
-            }
-        });
-        // otherwise show an error dialog when the update fails ^^^
+        data == "installed_plugin" || data == "overridden_plugin"
+            ? Dialog.show({
+                title: `Updated ${manifest['name']}`,
+                body: `Successfully updated to ${is_ghost_patch ? `build` : `version`} \`${type}\`. \nWould you like to reload Discord now?`,
+                confirmText: "Yep!",
+                cancelText: "Not now",
+                // reload discord from native function
+                onConfirm: () => { reload() },
+            })
+            : Dialog.show({
+                title: "Error",
+                body: `Something went wrong while updating ${manifest['name']}.`,
+                confirmText: "Report this issue",
+                cancelText: "Cancel",
+                onConfirm: () => {
+                    // @ts-ignore
+                    Router.openURL(`https://github.com/spinfal/enmity-plugins/issues/new?assignees=&labels=bug&template=bug_report.md&title=%5BBUG%5D%20${manifest['name']}%20Update%20Error%3A%20${!is_ghost_patch ? `v${version}` : `b${version}`}`);
+                },
+                onCancel: () => { Dialog.close() }
+            });
+        // otherwise log an issue when updating to console ^^^
     })
 }
 

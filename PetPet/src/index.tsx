78a6a40ebeda85e90ac2f38d8f5b420395ba51d6
spinfal <spin@seized.email>
2022-11-03 23:36:52 -0700

    petpet: better command handler

--- PetPet/src/index.tsx
+++ PetPet/src/index.tsx
@@ -1,20 +1,20 @@
 import { Plugin, registerPlugin } from 'enmity/managers/plugins';
 import manifest from '../manifest.json';
-import { pluginCommands } from './commands';
+import { commands } from './commands';
 import { React } from "enmity/metro/common";
 import SettingsPage from "../../common/components/_pluginSettings/settingsPage";
 
 const PetPet: Plugin = {
     ...manifest,
     onStart() {
-        this.commands = pluginCommands
+        this.commands = commands
     },
     onStop() {
         this.commands = []
     },
     patches: [],
     getSettingsPanel({ settings }): any {
-        return <SettingsPage manifest={manifest} settings={settings} hasToasts={false} section={null} commands={[{name: pluginCommands[0].name, description: pluginCommands[0].description}]} />;
+        return <SettingsPage manifest={manifest} settings={settings} hasToasts={false} section={null} commands={commands} />;
     },
 };
 
